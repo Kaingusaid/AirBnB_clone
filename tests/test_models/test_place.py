41f5170d36b67ab1f51768b478874b08d560b712
Kaingusaid <kaingusaid22@gmail.com>
2022-08-06 15:12:53 -0700

    place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest module for the Place Class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.place import Place
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
