783e6e61bc08c93eb00f565466f8f17e993bab1c
Kaingusaid <kaingusaid22@gmail.com>
2022-08-06 14:59:39 -0700

    test base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+ttest module for the BaseModel Class."""
+
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import json
+import os
+import re
+import time
+import unittest
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
