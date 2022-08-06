fb440a6540818097be08bedc38bdd9479cc0b9fe
Kaingusaid <kaingusaid22@gmail.com>
2022-08-06 15:30:29 -0700

    filr

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from models.engine.file_storage import FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
